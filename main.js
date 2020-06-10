4ed3773050ae749b1f9580b1f3ce9a3eba3dbd7e
GyeCheol <godmarketing1004@gamil.com>
2020-06-10 14:01:58 +0900

    JS: Home contact button

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     // console.log(window.scrollY);
     // console.log(`navbarHeight: ${navbarHeight}`);
-    if(window.scrollY > navbarHeight) {
+    if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
@@ -19,11 +19,23 @@ const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
     const link = target.dataset.link;
-    if (link == null ) {
+    if (link == null) {
         return;
     }
     console.log(event.target.dataset.link);
 
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({ behavior: 'smooth' });
-})
+    // const scrollTo = document.querySelector(link);
+    // scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollIntoView(link)
+});
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact')
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector)
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
