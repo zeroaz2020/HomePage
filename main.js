ef578ad8c7887dca0af52533af02805b9425d2e5
GyeCheol <godmarketing1004@gamil.com>
2020-06-10 15:08:15 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -40,10 +40,23 @@ homeContactBtn.addEventListener('click', () => {
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    home.style.opacity = 1 - window.scrollY / homeHeight;
+    home.style.opacity = 1 - (window.scrollY / homeHeight);
 });
 
+// Show "arrow up" button when scrolling
+const arrowUp = document.querySelector('.arrow-up')
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
 
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', ()=> {
+    scrollIntoView('#home');
+});
 
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector)
