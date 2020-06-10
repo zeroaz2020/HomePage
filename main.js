ede730ca3c4a88e881f024eb0f5c00e1e25ff51c
GyeCheol <godmarketing1004@gamil.com>
2020-06-10 17:10:36 +0900

    JS: Work filtering

--- main.js
+++ main.js
@@ -54,8 +54,37 @@ document.addEventListener('scroll', () => {
 });
 
 // Handle click on the "arrow up" button
-arrowUp.addEventListener('click', ()=> {
+arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
+});
+
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+
+    projectContainer.classList.add('animation-out');
+
+
+    setTimeout(() => {
+        projects.forEach((project) => {
+            // console.log(project);
+            console.log(project.dataset.type);
+
+            if (filter === '*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('animation-out');
+    }, 300);
 });
 
 function scrollIntoView(selector) {
